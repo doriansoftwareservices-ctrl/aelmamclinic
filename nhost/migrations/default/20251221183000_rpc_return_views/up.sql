360f5a18e0f68ea11d298072ccec61b29f6c2966
fa3550445-dotcom <fa3550445@gmail.com>
2025-12-21 19:38:50 +0300

    Drop all RPCs before changing return types

--- nhost/migrations/default/20251221183000_rpc_return_views/up.sql
+++ nhost/migrations/default/20251221183000_rpc_return_views/up.sql
@@ -4,6 +4,16 @@ DROP FUNCTION IF EXISTS public.my_profile();
 DROP FUNCTION IF EXISTS public.my_feature_permissions(uuid);
 DROP FUNCTION IF EXISTS public.admin_list_clinics();
 DROP FUNCTION IF EXISTS public.list_employees_with_email(uuid);
+DROP FUNCTION IF EXISTS public.my_account_id();
+DROP FUNCTION IF EXISTS public.admin_set_clinic_frozen(uuid, boolean);
+DROP FUNCTION IF EXISTS public.admin_delete_clinic(uuid);
+DROP FUNCTION IF EXISTS public.admin_create_owner_full(text, text, text);
+DROP FUNCTION IF EXISTS public.admin_create_employee_full(uuid, text, text);
+DROP FUNCTION IF EXISTS public.set_employee_disabled(uuid, uuid, boolean);
+DROP FUNCTION IF EXISTS public.delete_employee(uuid, uuid);
+DROP FUNCTION IF EXISTS public.chat_accept_invitation(uuid);
+DROP FUNCTION IF EXISTS public.chat_decline_invitation(uuid, text);
+DROP FUNCTION IF EXISTS public.chat_mark_delivered(uuid[]);
 
 CREATE OR REPLACE VIEW public.v_my_account_id AS
 SELECT NULL::uuid AS account_id
