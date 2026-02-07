a0bacaefdd6cd5dc998e3bbfc1bd9644f0986274
fa3550445-dotcom <fa3550445@gmail.com>
2026-02-07 20:49:20 +0300

    chore(nhost): remove owner-only auth.users locale migration

--- nhost/migrations/default/20260207172321_fix_auth_users_locale_default/down.sql
+++
@@ -1,2 +0,0 @@
-alter table auth.users
-  alter column locale drop default;
--- nhost/migrations/default/20260207172321_fix_auth_users_locale_default/up.sql
+++
@@ -1,7 +0,0 @@
--- Fix: auth.users.locale is NOT NULL without a default, which breaks dashboard user creation
-alter table auth.users
-  alter column locale set default 'en';
-
-update auth.users
-  set locale='en'
-where locale is null;
