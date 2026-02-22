cb3da69445b653fa9bec94efaa996191fff50faa
fa3550445-dotcom <fa3550445@gmail.com>
2026-02-23 00:36:01 +0300

    Make security_invoker chat views migration PG<15-safe

--- nhost/migrations/default/20260222203000_secure_chat_views_more/down.sql
+++ nhost/migrations/default/20260222203000_secure_chat_views_more/down.sql
@@ -1,3 +1,20 @@
-ALTER VIEW IF EXISTS public.v_chat_conversations_for_me SET (security_invoker = false);
-ALTER VIEW IF EXISTS public.v_chat_reads_for_me SET (security_invoker = false);
-ALTER VIEW IF EXISTS public.v_chat_messages_with_attachments SET (security_invoker = false);
+DO $$
+BEGIN
+  BEGIN
+    EXECUTE 'ALTER VIEW IF EXISTS public.v_chat_conversations_for_me SET (security_invoker = false)';
+  EXCEPTION WHEN others THEN
+    -- PG < 15 لا يدعم security_invoker للـ VIEWs، تجاهل الخطأ.
+  END;
+
+  BEGIN
+    EXECUTE 'ALTER VIEW IF EXISTS public.v_chat_reads_for_me SET (security_invoker = false)';
+  EXCEPTION WHEN others THEN
+    -- تجاهل في حال عدم الدعم.
+  END;
+
+  BEGIN
+    EXECUTE 'ALTER VIEW IF EXISTS public.v_chat_messages_with_attachments SET (security_invoker = false)';
+  EXCEPTION WHEN others THEN
+    -- تجاهل في حال عدم الدعم.
+  END;
+END $$;
